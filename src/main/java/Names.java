2b957e31bd27c65bc7e69047daeeaf3d8d5ee068
Andrius Juodelis <juodelis@puzzle.ch>
2016-09-15 15:58:40 +0200

    fetch names method

---
+++ src/main/java/Names.java
@@ -0,0 +1,15 @@
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Names {
+
+    public static List<String> getNames(String fileName) throws IOException {
+        ClassPathResource resource = new ClassPathResource(fileName);
+        return Files.lines(Paths.get(resource.getFile().getAbsolutePath())).collect(Collectors.toList());
+    }
+}
